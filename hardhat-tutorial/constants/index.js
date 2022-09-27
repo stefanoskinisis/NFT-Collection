5834eaf11e622e98dd83a1c66d253a02d01685db
stefanoskinisis <stefanoskinisis@gmail.com>
2022-09-27 14:21:17 +0300

    nft1

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x52bee1B40f2451927B8068149aCd4C7B011fa28B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dapp-my-app-xelh.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+//0xF5C5b07e52c03B1eD3cF2cc222516944AbbC2E8C
